9fe3d5ccf2db8e3e43ebf70425aab1c920a91a44
Michał Wadowski <wadosm@gmail.com>
2022-04-03 07:38:44 +0200

    Empty project

---
+++ src/main/java/com/example/genericio/GenericIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.genericio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/genericio/GenericIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genericio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
